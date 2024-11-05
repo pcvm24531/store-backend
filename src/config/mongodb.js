ea8cf03dabb7cba0a85613067d8cba6844ec0173
Cesar <csr2453@outlook.es>
2024-11-05 10:26:10 -0400

    Configuración adicional

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,15 +1,16 @@
 'use strict'
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const DB = 'storeBackend';
-const REF = 'mongodb://localhost:27017/';
+const DB = process.env.DB_NAME;
+const REF = process.env.DB_REF;
 
 async function connect() {
     await mongoose.connect( REF+DB, {
         useUnifiedTopology: true,
         useNewUrlParser:true,
     } );
-    console.log(`Database ${DB} connect...`);
+    console.log(`Database ${DB} connected...`);
 }
 
 module.exports = {
