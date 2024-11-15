829939b8a54d777c591865905a4cf428596fab01
Cesar <csr2453@outlook.es>
2024-11-15 17:00:23 -0400

    manejo de errores

--- src/product/product.db.js
+++ src/product/product.db.js
@@ -7,24 +7,46 @@ const DOCUMENT = 'product';
 let product = mongoose.model( DOCUMENT, schema.productSchema );
 
 async function save( data ) {
-    return await product.create( data );
+    try {
+        return await product.create( data );
+    } catch (error) {
+        throw new Error("file.db error:"+error);
+    }    
 }
 
 async function getAll() {
-    return await product.find();
+    try {
+        return await product.find();
+    } catch (error) {
+        throw new Error("file.db error:"+error);
+    }
 }
 
 async function getById(id) {
-    return await product.findById(id);
+    try {
+        const result = await product.findById(id);
+        if(!result) return {status:400, message:'No hay datos para el ID'};
+        return {status:200, result};
+    } catch (error) {
+        return {status:500, message:'Error:'+error.message};
+    }   
 }
 
 async function update( id, data ) {
-    await product.replaceOne( {_id: id}, data );
-    return getById(id);
+    try {
+        await product.replaceOne( {_id: id}, data );
+        return getById(id);
+    } catch (error) {
+        throw new Error("file.db error:"+error);
+    }
 }
 
 async function remove( id ) {
-    return await product.findByIdAndDelete( id );
+    try {
+        return await product.findByIdAndDelete( id );
+    } catch (error) {
+        throw new Error("file.db error:"+error);
+    }
 }
 
 module.exports = {
