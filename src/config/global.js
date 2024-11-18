a8aff522dd791501e4ebf5c2350e0948bcdf39ef
Cesar <csr2453@outlook.es>
2024-11-18 17:08:28 -0400

    Generación y envio de token

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,10 @@
 'use strict'
 
 const SALT_ROUNDS = 15;
+const KEY = 'nodejs';
+const COURSE = 'nodejs 2024'
 
 module.exports = {
-    SALT_ROUNDS
+    SALT_ROUNDS,
+    KEY
 }
--- src/middlewares/encrypt.middleware.js
+++ src/middlewares/encrypt.middleware.js
@@ -1,6 +1,7 @@
 'use strinc'
 const bcrypt = require('bcrypt');
-const { SALT_ROUNDS } = require('../config/global');
+const { SALT_ROUNDS, KEY } = require('../config/global');
+const jwt = require('jsonwebtoken');
 
 function encrypt(req, res, next) {
     const salt = bcrypt.genSaltSync( SALT_ROUNDS, );
@@ -9,7 +10,18 @@ function encrypt(req, res, next) {
     next();
 }
 
+//Funcion que permite o niega el acceso a un end point
+async function verifyToken(req, res, next) {
+    try {
+        const token = req.query.token;
+        jwt.verify(token, KEY);
+        next();
+    } catch (error) {
+        return res.status(400).json({msj:'Token no válido!'});
+    }
+}
 
 module.exports = {
-    encrypt
+    encrypt,
+    verifyToken
 }
